02b045c98343f5142b479bec3d4ec714948b3837
MuhammadShariqShoaib <134840447+MuhammadShariqShoaib@users.noreply.github.com>
2024-11-22 21:27:09 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://contentgen-ih6d.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
