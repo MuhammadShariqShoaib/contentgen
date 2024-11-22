c492a97f25614a85b8785e0897cd2a3324b2d26b
MuhammadShariqShoaib <134840447+MuhammadShariqShoaib@users.noreply.github.com>
2024-11-22 21:27:54 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://contentgen-ih6d.vercel.app"],
+        origin: ["https://contentgen-ih6d.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
